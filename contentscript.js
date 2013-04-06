d8ef2b246d0b0fc1a3617a2bfea059e17e649429
Hiroki Matsue <hiroki.matsue@gmail.com>
2013-04-06 16:29:28 +0900

    Add function to get Rakuten products.

--- contentscript.js
+++ contentscript.js
@@ -1,4 +1,35 @@
-function getKeyPhrase(text) {
+function insertRakutenProducts(phrase) {
+  var devid = "12e93771545fd01134acc95cb0e0f0f3";
+  var afid = "11058cd3.4711ade0.11058cd4.94807d6a";
+  var format = "xml";
+
+  $.ajax({
+    url:"https://app.rakuten.co.jp/services/api/IchibaItem/Search/20120723?applicationId=" + devid +
+      "&affiliateId=" + afid +
+      "&format=" + format +
+      "&keyword=" + phrase,
+    type:"get",
+    dataType:"xml",
+    timeout:1000,
+    cache:false,
+    error:function(){
+      alert("xmlファイルの読み込み失敗(rakuten)");
+    },
+    success:function(xml){
+      var item = $(xml).find("Item:first");
+      name = item.find("itemName").text();
+      catchcopy = item.find("catchcopy").text();
+      price = item.find("itemPrice").text();
+      url = item.find("affiliateUrl").text();
+      imgurl = item.find("imageUrl:first").text();
+
+      console.log("item:" + name);
+      console.log("item:" + imgurl);
+    }
+  }); 
+};
+
+function requestKeyPhrase(text) {
   var appid = "dj0zaiZpPW5jSFFvQVhYSnZENCZkPVlXazliekp0WW0xck4yVW1jR285TUEtLSZzPWNvbnN1bWVyc2VjcmV0Jng9NWY-";
   text = encodeURI(text.replace(/\s|　/g,"。"));
   console.log("replaced text:" + text);
@@ -11,12 +42,12 @@ function getKeyPhrase(text) {
     timeout:1000,
     cache:false,
     error:function(){
-      alert("xmlファイルの読み込み失敗");
+      alert("xmlファイルの読み込み失敗(yahoo)");
     },
     success:function(xml){
-      var res = $(xml).find("Keyphrase:first").text();
-      console.log("key phrase:" + res);
-      return res;
+      var phrase = $(xml).find("Keyphrase:first").text();
+      console.log("key phrase:" + phrase);
+      insertRakutenProducts(phrase);
     }
   });
 };
@@ -30,6 +61,6 @@ function getYouTubeTitle() {
 if (location.hostname == "www.youtube.com") {
   console.log("hostname:" + location.hostname);
   var title = getYouTubeTitle();
-  var phrase = getKeyPhrase(title);
+  requestKeyPhrase(title);
 };
 
