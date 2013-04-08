ef4a61fcf5e7db31b468cf6a1523986279013894
Rui Bando <bando.rui@gmail.com>
2013-04-09 01:05:34 +0900

    modify notification

--- notification.js
+++ notification.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', function(){
        data = JSON.parse(decodeURIComponent(data));   
     }
     if (data != 0) {
-		var html = '<img src="' + data.image + '">';
-    	html += '<p class="text">' + data.text + '<br>';
-        html += '価格：' + data.price + '円</p>';
+		var html = '<div class="itemimage"><img src="' + data.image + '" width="50px" height="50px"></div>';
+    	html += '<div class="itemtext"><p class="text">' + data.text + '<br>';
+        html += '価格：' + data.price + '円</p></div></div><a href="' + data.url + '" target="_blank">商品ページへ</a>';
         document.getElementById('content').innerHTML = html;
     }  
 });
