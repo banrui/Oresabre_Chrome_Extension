53cefcdaf6809f35ebec6fe54154a95de5973b51
Rui Bando <bando.rui@gmail.com>
2013-04-12 01:53:10 +0900

    オプションページ修正

--- background.js
+++ background.js
@@ -111,3 +111,9 @@ function requestKeyPhrase(text) {
   });
 }
 
+chrome.browserAction.onClicked.addListener(function(activeTab)
+{
+    var newURL = "options.html";
+    chrome.tabs.create({ url: newURL });
+});
+
